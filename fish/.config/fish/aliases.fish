d8a41f1497e7a371548530821bd127dee0b74596
Three <aurawastaken@gmail.com>
2025-05-09 13:51:41 +0200

    added fish aliases

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,7 @@
+alias untar "tar -xvzf"
+alias ls "ls --color=auto"
+alias ll "ls -l --color=auto"
+alias grep "grep --color=auto"
+alias upgrade "sudo pacman -Syu"
+alias update "sudo pacman -Syu"
+alias dots "cd ~/three-dotfiles && git status"
